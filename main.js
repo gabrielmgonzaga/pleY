87024314060d20bc785f343c2f0504c8f87b1541
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2016-12-06 14:39:48 -0800

    Working on data model layout.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let companies = [
         rating: 3,
         name: "illum qui",
         review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    }
+    },
   },
   {
     id: 1,
